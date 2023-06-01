e7d20201e5810313c550393b6a623852bfd61fab
khgad <khaled.626528@gmail.com>
2023-06-01 19:08:39 +0300

    edit key generated file permission

--- terraform/security/key.tf
+++ terraform/security/key.tf
@@ -11,6 +11,7 @@ resource "tls_private_key" "tls_key" {
 resource "local_file" "private_key_file" {
   content  = tls_private_key.tls_key.private_key_pem
   filename = "../ansible/key.pem"
+  file_permission = "0600"
 }
 
 resource "aws_key_pair" "tf_key" {
